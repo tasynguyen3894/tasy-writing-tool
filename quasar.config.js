72f954dc3f0d54e9268a8dfa51ff857076ad86dc
tasynguyen3894 <thaisang.nguyen3894@gmail.com>
2023-11-18 12:30:25 +0700

    feat: ✨ add config for updater

--- quasar.config.js
+++ quasar.config.js
@@ -189,7 +189,7 @@ module.exports = configure(function (/* ctx */) {
 
       inspectPort: 5858,
 
-      bundler: 'packager', // 'packager' or 'builder'
+      bundler: 'builder', // 'packager' or 'builder'
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
@@ -207,7 +207,11 @@ module.exports = configure(function (/* ctx */) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'write-tools'
+        appId: 'write-tools.sangnguyen.dev',
+        productName: 'Tasy Writing Tool',
+        win: {
+          target: 'nsis'
+        }
       }
     },
 
