05edc15b5794431aea7883410e5f8edcf9ed5a8b
tasynguyen3894 <thaisang.nguyen3894@gmail.com>
2023-11-20 22:39:14 +0700

    feat: ✨ add footer for the first page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,7 @@
     <q-header elevated :class="$q.dark.isActive ? 'bg-primary' : 'bg-black'">
       <q-toolbar>
         <q-toolbar-title >
-          Writing tool {{ appVersion }}
+          <div>Tasy Writing Tool</div>
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
@@ -12,6 +12,13 @@
       <router-view />
     </q-page-container>
     <UpdateVersion />
+    <q-footer reveal elevated class="bg-black">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div class="footer">V{{ appVersion }}</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
@@ -20,3 +27,9 @@ import UpdateVersion from 'src/components/UpdateVersion.vue';
 
 const appVersion = APP_VERSION;
 </script>
+<style scoped lang="scss">
+.footer {
+  text-align: center;
+  font-size: 12px;
+}
+</style>
